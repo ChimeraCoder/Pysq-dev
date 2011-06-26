d9e5ccfd3fbed5bee1e22773e188766f8eb944a1
Aditya Mukerjee <dev@chimeracoder.net>
2011-06-25 20:35:13 -0400

    Refactored code to enable authentication

--- pysq.py
+++ pysq.py
@@ -3,30 +3,55 @@ import urllib2
 import json
 import httplib
 
-CLIENT_ID = None#FILL IN THE CLIENT ID
-REDIRECT_URI = None#FILL IN THE REDIRECT URI
-CLIENT_SECRET = None #FILL IN THE CLIENT SECRET KEY
 
+class FSAuthenticator:
+    def __init__(self, client_id, client_secret, redirect_uri):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.redirect_uri = redirect_uri #same as callback_uri
+        self.access_token = None 
 
+    def authorize_uri():
+        return 'https://foursquare.com/oauth2/authenticate?client_id=' + CLIENT_ID + '&response_type=code&redirect_uri='+ REDIRECT_URI
 
-def authenticate():
-    return HttpResponseRedirect('https://foursquare.com/oauth2/authenticate?client_id=' + CLIENT_ID + '&response_type=code&redirect_uri='+ REDIRECT_URI)
+    def get_token(code):
+        pass
+        pass
+        #make http request to foursquare to get the access token from the code
+        #make urllib2 request to this url
+        "https://foursquare.com/oauth2/access_token?client_id=YOUR_CLIENT_ID&client_secret=YOUR_CLIENT_SECRET&grant_type=authorization_code&redirect_uri=YOUR_REGISTERED_REDIRECT_URI&code=CODE"
+        #Response will contain access token
+        self.access_token = access_token
+        return access_token
 
 def checkin(checkin_id):
         '''Given a checkin id, return the information for that checkin'''
 
+
+class UserFinder:
+    def __init__(self, authenticator):
+    '''Given an FSAuthenticator, search for a user'''
+        self.authenticator = authenticator
+
+    def findUser(self, id):
+        #Issue a get request to create and return a new user object
+        pass
+        return #return a new FSUser object
+
+
+
 class FSUser:
     
-    def __init__(self, code):
-        '''Given a code, query the user's id, username, and access token and store these'''
+    def __init__(self, authenticator, json_query):
+        '''Given a JSON query that describes the user, store the JSON for use at a later date. Also store the authenticator object for future queries'''
         response = urllib2.urlopen('https://foursquare.com/oauth2/access_token?client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&grant_type=authorization_code&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fauth&code=' + code)
 
         data = json.loads(response.read())
-        access_token = data['access_token']
-        self.id = data['response']['user']['id']
-        self.username = data['response']['user']['contact']['twitter']
-        self.access_token = data['access_token']
-        
+        self.authenticator = authenticator 
+
+    def fetch(self):
+        self.data = json.loads(########)
+
         #TODO cache this
     def first_name(self):
         '''Return the user's first name'''
